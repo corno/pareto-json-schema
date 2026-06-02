e0fb56118f2242e31f58ef5f7114eed2bc8c1f9f
Corno Schraverus <corno@schraverus.com>
2026-06-02 17:32:59 +0200

    pdt: ...

--- typescript/lib/src/implementation/manual/refiners/json_schema/json.ts
+++ typescript/lib/src/implementation/manual/refiners/json_schema/json.ts
@@ -4,28 +4,18 @@ import _p_cc from 'pareto-core/dist/_p_change_context'
 
 import * as d_in from "pareto-json/dist/interface/to_be_generated/json_with_parse_info"
 import * as d_out from "../../../../interface/to_be_generated/json_schema"
+import * as d_function from "pareto-json/dist/interface/to_be_generated/unmarshalled_from_json"
 
 import * as d_location from "astn-core/dist/interface/generated/liana/schemas/location/data"
 
 //dependencies
 import * as r_json_y from "pareto-json/dist/implementation/manual/refiners/json_y/json_with_parse_info"
+import * as r_json_x from "pareto-json/dist/implementation/manual/refiners/json_x/json_with_parse_info"
 
-export type My_Error = {
-    'type':
-    | ['unexpected type', {
-        'expected': _pi.List<string>
-    }]
-    | ['unexpected enum value', {
-        'expected': _pi.List<string>
-    }]
-    | ['multiple properties with this key', string]
-
-    'range': d_location.Range
-}
 
 export const Schema: _pi.Refiner<
     d_out.Schema,
-    My_Error,
+    d_function.Error,
     d_in.Value
 > = ($, abort) => {
     const range = $.range
@@ -33,36 +23,94 @@ export const Schema: _pi.Refiner<
         switch ($[0]) {
             case 'boolean': return _p.ss($, ($) => ['boolean', null])
             case 'object': return _p.ss($, ($): d_out.Schema => ['object', _p_cc(
-                r_json_y.Object_With_Unique_Keys(
+                r_json_x.Object_No_Unexpected_Properties_From_Object( //okay for now, but the JSON schema spec allows additional properties
                     $,
-                    ($) => abort({
-                        'type': ['multiple properties with this key', $['conflicting key']],
-                        'range': $['range'],
-                    })
+                    abort,
+                    {
+                        'expected properties': _p.dictionary.literal({
+                            "$id": null,
+                            "$defs": null,
+                            "$schema": null,
+                            "additionalProperties": null,
+                            "const": null,
+                            "maxProperties": null,
+                            "minLength": null,
+                            "minProperties": null,
+                            "properties": null,
+                            "title": null,
+                            "type": null,
+                            "uniqueItems": null,
+                            "oneOf": null,
+                            "allOf": null,
+                            "anyOf": null,
+                            "required": null,
+                        })
+                    }
                 ),
                 ($): d_out.Schema_Object => ({
+
+                    '$id': _p.optional.from.optional(
+                        $.properties.__get_possible_entry_deprecated("$id")
+                    ).map(($) => r_json_x.String($.value, abort).token.value),
+
+                    '$defs': _p.optional.from.optional(
+                        $.properties.__get_possible_entry_deprecated("$defs")
+                    ).map(($) => _p_cc(
+                        r_json_y.Object_With_Unique_Keys(
+                            r_json_x.Object($.value, abort),
+                            abort
+                        ),
+                        ($) => $.properties.__d_map(($) => Schema($.value, abort))
+                    )),
+
+                    '$ref': _p.optional.from.optional(
+                        $.properties.__get_possible_entry_deprecated("$ref")
+                    ).map(($) => r_json_x.String($.value, abort).token.value),
+
+                    '$schema': _p.optional.from.optional(
+                        $.properties.__get_possible_entry_deprecated("$schema")
+                    ).map(($) => r_json_x.String($.value, abort).token.value),
+
+                    'additionalProperties': _p.optional.from.optional(
+                        $.properties.__get_possible_entry_deprecated("additionalProperties")
+                    ).map(($) => Schema($.value, abort)),
 
                     'const': _p.optional.from.optional(
                         $.properties.__get_possible_entry_deprecated("const")
                     ).map(($) => $.value),
-                    
+
+                    'maxProperties': _p.optional.from.optional(
+                        $.properties.__get_possible_entry_deprecated("maxProperties")
+                    ).map(($) => r_json_x.Number($.value, abort).value),
+
+                    'minLength': _p.optional.from.optional(
+                        $.properties.__get_possible_entry_deprecated("minLength")
+                    ).map(($) => r_json_x.Number($.value, abort).value),
+
+                    'minProperties': _p.optional.from.optional(
+                        $.properties.__get_possible_entry_deprecated("minProperties")
+                    ).map(($) => r_json_x.Number($.value, abort).value),
+
+                    'properties': _p.optional.from.optional(
+                        $.properties.__get_possible_entry_deprecated("properties")
+                    ).map(($) => _p_cc(
+                        r_json_y.Object_With_Unique_Keys(
+                            r_json_x.Object($.value, abort),
+                            abort
+                        ),
+                        ($) => $.properties.__d_map(($) => Schema($.value, abort))
+                    )),
+
+                    'title': _p.optional.from.optional(
+                        $.properties.__get_possible_entry_deprecated("title")
+                    ).map(($) => r_json_x.String($.value, abort).token.value),
+
                     'type': _p.optional.from.optional(
                         $.properties.__get_possible_entry_deprecated("type")
                     ).map(($): d_out.Type => _p.decide.state($.value.type, ($) => {
                         switch ($[0]) {
-                            case 'string': return _p.ss($, ($) => ['single', Primitive($, abort)])
-                            case 'array': return _p.ss($, ($) => ['array', $.items.__l_map(($) => _p.decide.state($.type, ($) => {
-                                switch ($[0]) {
-                                    case 'string': return _p.ss($, ($) => Primitive($, abort))
-                                    default: return abort({
-                                        'type': ['unexpected type', {
-                                            'expected': _p.list.literal(['string'])
-                                        }],
-                                        'range': range,
-                                    })
-                                }
-                            }))])
-                            // case 'null':
+                            case 'string': return _p.ss($, ($) => ['single', Simple_Types($, abort)])
+                            case 'array': return _p.ss($, ($) => ['array', $.items.__l_map(($) => Simple_Types(r_json_x.String($, abort), abort))])
                             default: return abort({
                                 'type': ['unexpected type', {
                                     'expected': _p.list.literal(['string', 'array'])
@@ -71,6 +119,27 @@ export const Schema: _pi.Refiner<
                             })
                         }
                     })),
+
+                    'uniqueItems': _p.optional.from.optional(
+                        $.properties.__get_possible_entry_deprecated("uniqueItems")
+                    ).map(($) => r_json_x.Boolean($.value, abort).value),
+
+                    'required': _p.optional.from.optional(
+                        $.properties.__get_possible_entry_deprecated("required")
+                    ).map(($) => String_Array(r_json_x.Array($.value, abort), abort)),
+
+                    'oneOf': _p.optional.from.optional(
+                        $.properties.__get_possible_entry_deprecated("oneOf")
+                    ).map(($) => Schema_Array(r_json_x.Array($.value, abort), abort)),
+
+                    'anyOf': _p.optional.from.optional(
+                        $.properties.__get_possible_entry_deprecated("anyOf")
+                    ).map(($) => Schema_Array(r_json_x.Array($.value, abort), abort)),
+
+                    'allOf': _p.optional.from.optional(
+                        $.properties.__get_possible_entry_deprecated("allOf")
+                    ).map(($) => Schema_Array(r_json_x.Array($.value, abort), abort)),
+
                 })
             )])
             default: return abort({
@@ -83,9 +152,21 @@ export const Schema: _pi.Refiner<
     })
 }
 
-export const Primitive: _pi.Refiner<
-    d_out.Primitive,
-    My_Error,
+export const Schema_Array: _pi.Refiner<
+    d_out.Schema_Array,
+    d_function.Error,
+    d_in.Array
+> = ($, abort) => $.items.__l_map(($) => Schema($, abort))
+
+export const String_Array: _pi.Refiner<
+    d_out.String_Array,
+    d_function.Error,
+    d_in.Array
+> = ($, abort) => $.items.__l_map(($) => r_json_x.String($, abort).token.value)
+
+export const Simple_Types: _pi.Refiner<
+    d_out.Simple_Types,
+    d_function.Error,
     d_in.String
 > = ($, abort) => {
     switch ($.token.value) {
--- typescript/lib/src/interface/to_be_generated/json_schema.ts
+++ typescript/lib/src/interface/to_be_generated/json_schema.ts
@@ -8,15 +8,36 @@ export type Schema =
     | ['object', Schema_Object]
 
 export type Schema_Object = {
+    '$id': _pi.Optional_Value<string>
+    '$defs': _pi.Optional_Value<_pi.Dictionary<Schema>>
+    '$ref': _pi.Optional_Value<string>
+    '$schema': _pi.Optional_Value<string>
+    'additionalProperties': _pi.Optional_Value<Schema>
     'const': _pi.Optional_Value<d_json.Value>
+    'maxProperties': _pi.Optional_Value<number>
+    'minLength': _pi.Optional_Value<number>
+    'minProperties': _pi.Optional_Value<number>
+    'properties': _pi.Optional_Value<Properties>
+    'required': _pi.Optional_Value<String_Array>
+    'title': _pi.Optional_Value<string>
     'type': _pi.Optional_Value<Type>
+    'uniqueItems': _pi.Optional_Value<boolean>
+    'oneOf': _pi.Optional_Value<Schema_Array>
+    'allOf': _pi.Optional_Value<Schema_Array>
+    'anyOf': _pi.Optional_Value<Schema_Array>
 }
 
-export type Type =
-    | ['array', _pi.List<Primitive>]
-    | ['single', Primitive]
+export type Properties = _pi.Dictionary<Schema>
 
-export type Primitive =
+export type Type =
+    | ['array', _pi.List<Simple_Types>]
+    | ['single', Simple_Types]
+
+export type String_Array = _pi.List<string>
+
+export type Schema_Array = _pi.List<Schema>
+
+export type Simple_Types =
     | ['array', null]
     | ['boolean', null]
     | ['null', null]
