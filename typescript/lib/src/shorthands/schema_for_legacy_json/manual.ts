bea8528d92eace33d36997d0d740187ce2795a79
Corno Schraverus <corno@schraverus.com>
2026-07-10 10:05:49 +0200

    pdt: ...

--- typescript/lib/src/shorthands/schema_for_legacy_json/manual.ts
+++ typescript/lib/src/shorthands/schema_for_legacy_json/manual.ts
@@ -1,6 +1,6 @@
 import * as p_ from 'pareto-core-shorthands/unconstrained_manual'
 
-import * as x from "../../interface/generated/liana/schemas/schema_for_legacy_json/data.js"
+import * as x from "../../interface/data/schema_for_legacy_json.js"
 
 
 export const document = (
